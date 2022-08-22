94fcd352bf6e61447721c15d5ae58f32885d717a
sarah <skg@neofilm.us>
2022-08-22 18:26:35 -0400

    Add functionality-program dropdown list to display

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(
+        `https://randomuser.me/api?results=${numUsers}`
+    );
     const data = await usersRequest.json();
 
     const userResults = data.results; // array of objects
     displayUsers(userResults);
 };
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
     // Clear the randomFolks element
@@ -22,8 +25,13 @@ const displayUsers = function (userResults) {
         userDiv.innerHTML = `
             <h3>${name}</h3>
             <p>${country}</p>
-            <img src=${imageURL} alt ="user avater" />
-        `;
+            <img src=${imageURL} alt ="user avatar" />
+            `;
         randomFolks.append(userDiv);
     }
 };
+
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
